4b7a9e8a15eb7c7ead1890b34506b438718f720b
alayoff <76528702+alayoff@users.noreply.github.com>
2020-12-23 21:33:22 +0200

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,14 @@
+temperature = float(input("Enter the degree number: "))
+temperature_type = str(input("if it's Celsius type \"C\":\nif it's Fahrenheit type \"F\": ")).upper()
+celsius = temperature
+fahrenheit = temperature
+
+if temperature_type == 'F' or 'f':
+    print((5 * fahrenheit - 160) / 9,'C')
+elif temperature_type == 'C' or 'C':
+    print((9*celsius+32*5) / 5,'F')
+else:
+    print('Oops, wrong imput\nPlease try again')
+
+
+
